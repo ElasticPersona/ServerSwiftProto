2f9091ae2e6a60319d9ebeb91ea07404fb6d1f5d
Shuhei Hasegawa <rivaldo@xa2.so-net.ne.jp>
2016-02-21 09:03:49 +0900

    Initial Commit

---
+++ ServerSwiftProto/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ServerSwiftProto
+//
+//  Created by Shuhei Hasegawa on 2016/02/21.
+//  Copyright © 2016年 Shuhei Hasegawa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
